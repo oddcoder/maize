8b1124967554012f5fb4a3bec4ee065592a5f7ea
oddcoder <ahmedsoliman@oddcoder.com>
2017-07-27 20:13:11 +0200

    removing pdb

--- dfs.py
+++ dfs.py
@@ -13,6 +13,4 @@ def dfs(graph, weights, start, end):
         for child in childs:
             parents[child] = node
             unexplored.append(child)
-    import pdb
-    pdb.set_trace()
     return get_path(parents, start, end)
